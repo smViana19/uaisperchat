808138b54589cac1c002314c77337adb7b4f9647
Samuu <samuelfilipevianadois@gmail.com>
2026-03-10 10:30:22 -0300

    Initial commit

---
+++ src/main/java/br/com/smvtech/uaisperchat/UaisperchatApplication.java
@@ -0,0 +1,13 @@
+package br.com.smvtech.uaisperchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UaisperchatApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UaisperchatApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/smvtech/uaisperchat/UaisperchatApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smvtech.uaisperchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaisperchatApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
